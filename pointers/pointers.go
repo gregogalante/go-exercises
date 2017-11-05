8c83c008c68249d701572f5e4b8f4c56ad6860cb
Gregorio Galante <gregogalante@gmail.com>
2017-11-05 20:43:33 +0100

    add errors example

--- pointers/pointers.go
+++ pointers/pointers.go
@@ -12,20 +12,20 @@ func zeroPoint(point *int) {
 
 func main() {
 	i := 1
-	fmt.Println("Initial value is: ", i) // -> 1
-	fmt.Println("Initial pointer value is: ", &i) // -> 0xc420018068
+	fmt.Println("Initial value is:", i) // -> 1
+	fmt.Println("Initial pointer value is:", &i) // -> 0xc420018068
 
 	zeroVal(i)
-	fmt.Println("After zeroVal value is: ", i) // -> 1
-	fmt.Println("After zeroVal pointer value is: ", &i) // -> 0xc420018068
+	fmt.Println("After zeroVal value is:", i) // -> 1
+	fmt.Println("After zeroVal pointer value is:", &i) // -> 0xc420018068
 	/*
 	zeroVal can not update the "i" value because it receives a copy of "i" value called "val"
 	in a different memeory location.
 	*/
 
 	zeroPoint(&i)
-	fmt.Println("After zeroPoint value is: ", i) // -> 0
-	fmt.Println("After zeroPoint pointer value is: ", &i) // -> 0xc420018068
+	fmt.Println("After zeroPoint value is:", i) // -> 0
+	fmt.Println("After zeroPoint pointer value is:", &i) // -> 0xc420018068
 	/*
 	zeroPoint can update the "i" value because it receives a pointer to the memory location
 	of the "i" value and it updates the value on this location.
