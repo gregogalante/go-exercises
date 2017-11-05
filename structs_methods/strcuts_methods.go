28f1500cc964cbbb2d5a62b7926e2cca12a792b2
Gregorio Galante <gregogalante@gmail.com>
2017-11-05 17:56:20 +0100

    added structs methods

---
+++ structs_methods/strcuts_methods.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+type rect struct {
+	width, height int
+}
+
+func (r *rect) area() int {
+	return r.width * r.height
+}
+
+func (r *rect) perim() int {
+	return 2 * r.width + 2 * r.height
+}
+
+func main() {
+	r := rect{10, 5}
+	fmt.Println(r)
+
+	fmt.Println(r.area())
+	fmt.Println(r.perim())
+}
