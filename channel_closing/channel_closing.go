b9197d0796efb543fcd14fed6181e1da7894c5c5
Gregorio Galante <gregogalante@gmail.com>
2017-12-14 09:26:35 +0100

    completed channel closing example

--- channel_closing/channel_closing.go
+++ channel_closing/channel_closing.go
@@ -3,5 +3,29 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("TODO")
+	jobs := make(chan int, 5)
+	done := make(chan bool)
+
+	go func() {
+		for {
+			j, more := <- jobs
+			if more {
+				fmt.Println("Received job", j)
+			} else {
+				fmt.Println("All jobs done")
+				done <- true
+				return
+			}
+		}
+	}()
+
+	for j := 1; j <= 3; j++ {
+		jobs <- j
+		fmt.Println("Sent job", j)
+	}
+
+	close(jobs)
+	fmt.Println("Sent all jobs")
+
+	<- done
 }
